783697902d2c919aa5e0b8690e78ca781f818cd9
carlogino <carloginocatapang@gmail.com>
2021-07-17 10:25:16 +0800

    feat: Created proxy file

--- next.config.js
+++ next.config.js
@@ -1,3 +1,12 @@
 module.exports = {
   reactStrictMode: true,
-}
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination:
+          "https://storage.googleapis.com/coding_challenge_assets/:path*",
+      },
+    ];
+  },
+};
