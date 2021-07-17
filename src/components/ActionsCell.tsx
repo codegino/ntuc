7661fbbd04a99f9dd7913d729e27bf863a720767
carlogino <carloginocatapang@gmail.com>
2021-07-17 15:57:29 +0800

    feat(table): Added easy to use pagination

--- src/components/ActionsCell.tsx
+++ src/components/ActionsCell.tsx
@@ -7,12 +7,14 @@ const EllipsisButton = styled.button`
   background-image: url("https://storage.googleapis.com/coding_challenge_assets/vertical-ellipsis.svg");
   background-repeat: no-repeat;
   background-position: center;
+  border: none;
+  background-color: transparent;
   cursor: pointer;
 `;
 
 export const ActionsCell = ({ row }: UseTableCellProps<Coupon>) => {
   return (
-    <div style={{ margin: "auto" }}>
+    <div style={{ margin: "auto", textAlign: "right" }}>
       <EllipsisButton
         role="button"
         onClick={() => window.alert(row.original.couponCode)}
--- src/components/CustomPagination.tsx
+++ src/components/CustomPagination.tsx
@@ -1,17 +1,13 @@
 import { TableInstance } from "react-table";
 import { Coupon } from "../models/coupon.model";
 import { useMediaQueries } from "@react-hook/media-query";
+import Pagination from "@material-ui/lab/Pagination";
 
 export const CustomPagination = ({
   pageOptions,
   rows,
-  previousPage,
-  canPreviousPage,
   gotoPage,
-  pageCount,
-  nextPage,
-  canNextPage,
-  state: { pageIndex, pageSize },
+  state: { pageIndex },
 }: TableInstance<Coupon>) => {
   const { matchesAll } = useMediaQueries({
     screen: "screen",
@@ -28,26 +24,13 @@ export const CustomPagination = ({
       <div>
         Showing {pageIndex + 1} - {pageOptions.length} of {rows.length}
       </div>
-      <div>
-        <button onClick={() => previousPage()} disabled={!canPreviousPage}>
-          {"<"}
-        </button>{" "}
-        {Array(5)
-          .fill(null)
-          .map((_, index) => {
-            return (
-              <button onClick={() => gotoPage(index + pageIndex)} key={index}>
-                {index + pageIndex + 1}
-              </button>
-            );
-          })}
-        <button onClick={() => gotoPage(pageCount - 1)} disabled={!canNextPage}>
-          {pageCount}
-        </button>{" "}
-        <button onClick={() => nextPage()} disabled={!canNextPage}>
-          {">"}
-        </button>{" "}
-      </div>
+
+      <Pagination
+        count={pageOptions.length}
+        variant="outlined"
+        shape="rounded"
+        onChange={(_, _page) => gotoPage(_page - 1)}
+      />
     </div>
   );
 };
